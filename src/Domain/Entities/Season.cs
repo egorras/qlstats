50379f2481619781b523767d52808dc6e2ab7f1d
Egor Raskhodchikov <raskhodchikov.egor@gmail.com>
2023-12-22 15:34:19 +0100

    season pts

--- src/Domain/Entities/Season.cs
+++ src/Domain/Entities/Season.cs
@@ -5,6 +5,9 @@ public class Season : BaseEntity
     public DateTime StartsAt { get; set; }
     public DateTime EndsAt { get; set; }
 
+    public int PtsForClanArenaRoundWin { get; set; } = 1;
+    public int PtsForClanArenaMatchWin { get; set; } = 3;
+
     public override string ToString()
     {
         return $"Season #{Id} ({StartsAt:yyyy.MM.dd} - {EndsAt:yyyy.MM.dd})";
---
+++ src/Infrastructure/Data/Migrations/20231222143327_AddSeasonPtsSetting.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QLStats.Infrastructure.Migrations;
+
+/// <inheritdoc />
+public partial class AddSeasonPtsSetting : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<int>(
+            name: "PtsForClanArenaMatchWin",
+            table: "Seasons",
+            type: "int",
+            nullable: false,
+            defaultValue: 0);
+
+        migrationBuilder.AddColumn<int>(
+            name: "PtsForClanArenaRoundWin",
+            table: "Seasons",
+            type: "int",
+            nullable: false,
+            defaultValue: 0);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "PtsForClanArenaMatchWin",
+            table: "Seasons");
+
+        migrationBuilder.DropColumn(
+            name: "PtsForClanArenaRoundWin",
+            table: "Seasons");
+    }
+}
