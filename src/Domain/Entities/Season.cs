b162d02422187a5073c067ac7009dbfab4254079
Egor Raskhodchikov <raskhodchikov.egor@gmail.com>
2025-01-16 15:54:29 +0100

    update

--- src/Domain/Entities/Season.cs
+++ src/Domain/Entities/Season.cs
@@ -5,8 +5,8 @@ public class Season : BaseEntity
     public DateTime StartsAt { get; set; }
     public DateTime EndsAt { get; set; }
     public string Name { get; set; } = string.Empty;
-    public int PtsForClanArenaRoundWin { get; set; } = 1;
-    public int PtsForClanArenaMatchWin { get; set; } = 3;
+    public decimal PtsForClanArenaRoundWin { get; set; } = 1;
+    public decimal PtsForClanArenaMatchWin { get; set; } = 3;
     public bool UseGameScore { get; set; } = false;
     public decimal? DamageForOnePts { get; set; } = 100;
     public int PtsPerMedal { get; set; } = 0;
---
+++ src/Infrastructure/Data/Migrations/20250116145418_UpdateData.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QLStats.Infrastructure.Data.Migrations;
+
+/// <inheritdoc />
+public partial class UpdateData : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<decimal>(
+            name: "PtsForClanArenaRoundWin",
+            schema: "QLSTATS",
+            table: "Seasons",
+            type: "numeric",
+            nullable: false,
+            oldClrType: typeof(int),
+            oldType: "integer");
+
+        migrationBuilder.AlterColumn<decimal>(
+            name: "PtsForClanArenaMatchWin",
+            schema: "QLSTATS",
+            table: "Seasons",
+            type: "numeric",
+            nullable: false,
+            oldClrType: typeof(int),
+            oldType: "integer");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<int>(
+            name: "PtsForClanArenaRoundWin",
+            schema: "QLSTATS",
+            table: "Seasons",
+            type: "integer",
+            nullable: false,
+            oldClrType: typeof(decimal),
+            oldType: "numeric");
+
+        migrationBuilder.AlterColumn<int>(
+            name: "PtsForClanArenaMatchWin",
+            schema: "QLSTATS",
+            table: "Seasons",
+            type: "integer",
+            nullable: false,
+            oldClrType: typeof(decimal),
+            oldType: "numeric");
+    }
+}
